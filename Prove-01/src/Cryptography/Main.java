42b50c437917335263456e20de4c000fcc0faf1a
Collin Blake <collinblake1@gmail.com>
2019-04-22 21:13:56 -0600

    Updates

---
+++ Prove-01/src/Cryptography/Main.java
@@ -0,0 +1,8 @@
+package Cryptography;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ Prove-01/src/Cryptography/User.java
@@ -0,0 +1,11 @@
+package Cryptography;
+
+public class User {
+
+    //variables
+    private String password;
+    private String salt;
+    private String hash;
+
+
+}
